9515e89a2a9c8161c214c34f83c4eebc01769f8e
wahyuhidayat1911 <whyuhidayat1911@gmail.com>
2023-07-05 18:04:49 +0700

    initial commit

---
+++ page_slide/assets/js/script_slick.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  
+  $(".Modern-Slider").slick({
+    autoplay:true,
+    autoplaySpeed:3000,
+    speed:2000,
+    slidesToShow:1,
+    slidesToScroll:1,
+    pauseOnHover:false,
+    dots:false,
+    pauseOnDotsHover:false,
+    cssEase:'linear',
+    fade:true,
+    draggable:false,
+   arrows: false,
+  });
+  
+})
